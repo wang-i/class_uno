b246f8598e7f1bbd581dbcb3b5a3b3ec0f9596fc
Bel Wang <belwang@cmail.carleton.ca>
2024-11-25 21:24:47 -0500

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,84 @@
 import logo from './logo.svg';
 import './App.css';
+import { classNames } from './components/class-info';
+import { classImages } from './components/class-info';
+import { classQuotes } from './components/class-info';
+import { posEffects } from './components/class-info';
+import { negEffects } from './components/class-info';
+import { softColors } from './components/colors';
+import { brightColors } from './components/colors';
+import { useState } from 'react';
+import { useEffect } from 'react';
+import { useRef } from 'react';
 
 function App() {
+  // Initial state
+  const [color, setColor] = useState("#ec3939");
+  const [clazz, setClass] = useState("OMG Class Uno!!!!!");
+  const [image, setImage] = useState(logo);
+  const [quote, setQuote] = useState("woahhhhhhhhhh wow amazing ඞ");
+  const [posEffect, setPosEffect] = useState("+ fun");
+  const [negEffect, setNegEffect] = useState("- brain explode");
+
+  // State change after reload
+  const handleClick = () => {
+    const randNum = Math.floor(Math.random() * classNames.length);
+    
+    setClass(classNames[randNum]);
+    setImage(classImages[randNum]);
+    setQuote(classQuotes[randNum]);
+
+    if (randNum == 9) { // Time traveller has a longer list of positive effects
+      setPosEffect(posEffects[randNum*3] + "\n" + posEffects[randNum*3 + 1] + "\n" + posEffects[randNum*3 + 2]);
+    } else {
+      setPosEffect(posEffects[randNum*3] + "\n" + posEffects[randNum*3 + 1]);
+    }
+    
+    if (randNum == 0) { // Traveller has no negative effects
+      setNegEffect("");
+    } else {
+    setNegEffect(negEffects[randNum*2] + "\n" + negEffects[randNum*2 + 1]);
+    }
+
+    // Change background color
+    const randColor = randNum % softColors.length; // each class should have a consistent colour
+    setColor(softColors[randColor]);
+  };
+
+  useEffect(() => { //https://ghost-together.medium.com/how-to-change-background-color-dynamically-in-react-on-mouse-click-8af02fdc5e95
+    document.body.style.backgroundColor = color
+  }, [color]) //[color] is a dependency array which tells React to only re-run the useEffect hook when the color variable changes (according to chatgpt)
+
+  // Allow users to use Space or Enter instead of clicking button
+  const documentRef = useRef(document); //https://www.preciousorigho.com/articles/a-better-way-to-create-event-listeners-in-react
+  documentRef.current.addEventListener("keydown", (e) => {
+    onkeydown = (e) => {
+      if (e.key == 'Enter' || e.key == " ") {
+        handleClick();
+      };
+    }
+  });
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="Card">
+        <div className="Title">
+          <h3>{clazz}</h3>
+        </div>
+        <div className="Image">
+          <img src={image} className="App-logo" alt="logo" />
+        </div>
+        <div className="Quote">
+          <h4>{quote}</h4>
+        </div>
+          <div className="Description">
+            <div className="Description-Container">
+              <p id="class-description-pos">{posEffect}</p>
+              <p id="class-description-neg">{negEffect}</p>
+            </div>
+          </div>
+      </div>
+      <button onClick={handleClick}>Generate class</button>
     </div>
   );
 }
---
+++ src/components/colors.js
@@ -0,0 +1,15 @@
+export const softColors = [
+  "#c42038",
+  "#ecd12c",
+  "#5ba93b",
+  "#0054a4",
+  "#eee9ef",
+  "#130f0e"
+]
+
+export const brightColors = [
+  "#e51e25",
+  "#fedc00",
+  "#38a647",
+  "#006cb7"
+]
